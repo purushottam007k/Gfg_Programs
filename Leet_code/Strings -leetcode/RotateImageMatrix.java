48772c8970dff6763ce3be34ed9f5936b83a5c2f
Purushottam kakade <63189371+purushottam007k@users.noreply.github.com>
2023-06-22 06:41:02 +0530

    Add files via upload

---
+++ Leet_code/Strings -leetcode/RotateImageMatrix.java
@@ -0,0 +1,38 @@
+
+public class RotateImageMatrix {
+	public static void rotate(int[][] matrix) {
+        int  k=matrix.length-1;
+       int [][] swapMatrix = new int [matrix.length][matrix.length]; 
+       int fix = matrix.length -1;
+       
+       for (int i = 0; i < swapMatrix.length; i++) {
+           for (int j = swapMatrix.length-1; j >=0; j--) {
+               swapMatrix[i][fix-j]= matrix[k][fix-j];                
+           }
+           k--;
+       }
+
+       for (int i = 0; i < swapMatrix.length; i++) {
+           for (int j = 0; j < swapMatrix.length; j++) {                
+               matrix[i][j]=swapMatrix[j][i];
+           }
+       }
+   
+
+	for(int n=0;n<matrix.length;n++)
+	{
+		for(int a:matrix[n])
+		{
+			System.out.print(a+" ");
+		}
+		System.out.println();
+	}
+	}
+	
+	
+	public static void main(String[] args) {
+		int matrix[][]= {{1,2,3},{4,5,6},{7,8,9}};
+		rotate(matrix);
+	}
+
+}
